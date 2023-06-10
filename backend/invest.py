bcb96e7920032850bf1ea4d9de946489e697b44d
Ofir Matuti <ofir.matuti@doubleverify.com>
2023-06-10 11:49:53 +0300

    add interval

--- backend/invest.py
+++ backend/invest.py
@@ -19,22 +19,22 @@ def get_investments():
     except ValueError:
         return jsonify({'error': 'Invalid start_month format. Please use YYYY-MM-DD.'}), 400
 
-    monthly_investment = int(request.args.get('monthly_investment', 1000))
-
-    sp500_data = yf.download('^GSPC', start=start_month, end=datetime.date.today().isoformat(), interval='1mo')
+    interval_investment = int(request.args.get('investment', 1000))
+    interval = request.args.get('interval', '3mo')
+    stock_data = yf.download('^GSPC', start=start_month, end=datetime.date.today().isoformat(), interval=interval)
 
     investment_list = []
 
     total_net = 0
     total_invested = 0
-    last_price = sp500_data['Open'][0]
+    last_price = stock_data['Open'][0]
     locale.setlocale(locale.LC_ALL, '')
-    for date, row in sp500_data.iterrows():
+    for date, row in stock_data.iterrows():
         open_price = row['Open']
         roi = open_price / last_price
         last_price = open_price
-        total_net = total_net * roi + monthly_investment
-        total_invested += monthly_investment
+        total_net = total_net * roi + interval_investment
+        total_invested += interval_investment
 
         formatted_total_net = locale.format_string('%.2f', total_net, grouping=True)
         formatted_total_invested = locale.format_string('%.2f', total_invested, grouping=True)
